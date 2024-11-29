5d133a9ff4179c4c5d385f2b5ad905cb15c938f1
MHaseemZahid <haseem.zahid@gmail.com>
2024-11-29 22:14:22 +0500

    task 2 Ask number and display positive or negitive

--- app.js
+++ app.js
@@ -30,3 +30,37 @@ else {  console.log("You are fail")
     
 }
 
+
+
+// 
+//var ask_num_to_dis ;
+
+// var ret_val;
+
+// function fun_ask_num_to_dis(ask_num_to_dis) {
+    
+//     if (ask_num_to_dis>0) {
+//        ret_val="Positive  value";
+//        }
+//     else {   
+// if (ask_num_to_dis===0) {
+//             ret_val="Zero  value";
+//            }
+// else  {
+//             ret_val="Negaitve  value";
+//            }
+     
+
+
+//     }
+
+//     return (ret_val);
+    
+// }
+
+
+// ;
+// var fun_val=+prompt("Enter Your number" );
+
+// var fun_ret_val = fun_ask_num_to_dis(fun_val);
+// console.log( fun_ret_val);
