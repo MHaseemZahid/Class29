1b9e6df77959d14f569fb82847129d4f9e135b6d
MHaseemZahid <haseem.zahid@gmail.com>
2024-11-29 22:13:22 +0500

    task 1 Ask % and display  grade

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-alert("test");
+//ask % to display grade
+  
+var ask_per = +prompt("Enter Your Percentage" ,90);
+
+  if (ask_per>85) {
+    console.log("Your Grade is A+")
+} 
+
+else if (ask_per>75) {
+    console.log("Your Grade is A")
+} 
+
+
+else if (ask_per>65) {
+    console.log("Your Grade is B")
+} 
+
+
+else if (ask_per>55) {
+    console.log("Your Grade is C")
+} 
+
+
+else if (ask_per>45) {
+    console.log("Your Grade is D")
+} 
+
+
+else {  console.log("You are fail")
+    
+}
+
